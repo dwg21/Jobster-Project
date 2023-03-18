074393f0b1a3da37b5b5b31f2f62e308d6e2a75e
dwg21 <95627833+dwg21@users.noreply.github.com>
2023-03-18 15:31:21 -0500

    First Commit

---
+++ populate.js
@@ -0,0 +1,24 @@
+
+
+// populates the database with dummydate for teh dummy acount
+
+require('dotenv').config();
+
+const mockData = require('./mock-data.json');
+
+const Job = require('./models/Job');
+const connectDB = require('./db/connect');
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        await Job.create(mockData)
+        console.log('Sucesss')
+        process.exit(0);
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+    }
+};
+
+start()
